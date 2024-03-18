63924d42a7009a2b7a9b435f9ba826cee6e8cd77
André Machado <andrels.machado82@gmail.com>
2024-03-18 19:21:21 -0300

    Interceptor para incluir token nas requisições

---
+++ src/interceptors/auth-interceptor.ts
@@ -0,0 +1,31 @@
+import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
+import { API_CONFIG } from "../config/api.config";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        let localUser = this.storage.getLocalUser();
+        let sizeUrl = API_CONFIG.baseUrl.length;
+        let requestParaApi = req.url.substring(0, sizeUrl) == API_CONFIG.baseUrl;
+
+        if (localUser && requestParaApi) {
+            const authRequest = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token)});
+            return next.handle(authRequest);
+        } else {
+            return next.handle(req)
+        }
+    }
+}   
+
+export const AuthInterceptorProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+};
