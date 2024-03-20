27b3a889e0d6d5fe727538f2a1679bf8ca68633f
André Machado <andrels.machado82@gmail.com>
2024-03-19 21:04:34 -0300

    Carregando imagens dos produtos do buketS3 da amazon

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -22,7 +23,18 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(idCategoria)
     .subscribe(response => {
       this.listaProdutos = response['content'];
+      this.getImageIfExistsFromBucket();
     }, error => {});
+  }
+
+  getImageIfExistsFromBucket() {
+    for (let i = 0; i < this.listaProdutos.length; i++) {
+      let item = this.listaProdutos[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+      .subscribe(response => {
+        item.imageUrl = `${API_CONFIG.bucketS3BaseUrl}/prod${item.id}-small.jpg`;
+      }, error => {})      
+    }
   }
 
 }
