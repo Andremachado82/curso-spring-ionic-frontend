0614b4ff6778b46272d1cf940ba3e270241a9b52
André Machado <andrels.machado82@gmail.com>
2024-03-21 19:42:13 -0300

    Tela de escolha de pagamento

--- src/pages/escolher-endereco/escolher-endereco.ts
+++ src/pages/escolher-endereco/escolher-endereco.ts
@@ -51,7 +51,7 @@ export class EscolherEnderecoPage {
 
   nextPage(item: EnderecoDTO) {
     this.pedido.enderecoDeEntrega = {id: item.id};
-    console.log(this.pedido);
+    this.navCtrl.push('PagamentoPage', {pedido: this.pedido});
   }
 
 }
