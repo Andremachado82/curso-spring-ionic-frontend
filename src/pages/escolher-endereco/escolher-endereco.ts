208622b88d2e81c5f9d6c255cecd70c66d197e90
André Machado <andrels.machado82@gmail.com>
2024-03-21 19:18:59 -0300

    Armazenamento dos dados do pedido

--- src/pages/escolher-endereco/escolher-endereco.ts
+++ src/pages/escolher-endereco/escolher-endereco.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { StorageService } from '../../services/storage.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 
 @IonicPage()
 @Component({
@@ -13,10 +15,13 @@ export class EscolherEnderecoPage {
 
   items: EnderecoDTO[];
 
+  pedido: PedidoDTO;
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public storageService: StorageService,
-    public clienteService:  ClienteService) {
+    public clienteService:  ClienteService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -25,10 +30,28 @@ export class EscolherEnderecoPage {
       this.clienteService.findByEmail(localUser.email)
       .subscribe(response => {
         this.items = response['enderecos'];
+
+        let cart = this.cartService.getCart();
+
+        this.pedido = {
+          cliente: {id: response['id']},
+          enderecoDeEntrega: null,
+          pagamento: null,
+          itens: cart.itens.map(i => {
+            return {
+              quantidade: i.quantidade, produto: {id: i.produto.id}
+            }
+          })
+        }
       }, error => {
         
       });
     }
+  }
+
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
   }
 
 }
