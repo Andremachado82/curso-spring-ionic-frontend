69d61744ae0db84886d29c5cb1f17d30497ed5a4
André Machado <andrels.machado82@gmail.com>
2024-03-21 17:54:11 -0300

    Acesso ao carrinho: botão flutuante e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,7 @@ export class MyApp {
     this.pages = [
       { title: 'Perfil', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Carrinho', component: 'CartPage'},
       { title: 'Sair', component: '' }
     ];
 
