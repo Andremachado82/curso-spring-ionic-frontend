80d6d9bca728db24824404108ad12f737ca28505
André Machado <andrels.machado82@gmail.com>
2024-03-23 16:40:43 -0300

    Bugfix- Cancelamento da camera ou galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:8081',
+    baseUrl: 'http://localhost:8080',
     bucketS3BaseUrl: 'https://curso-spring-andre.s3.sa-east-1.amazonaws.com'
 }
